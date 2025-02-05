b79e77499d9bfa8f463ae4cea25795af669544b4
hidoKim <kimdoyeon0113@naver.com>
2025-02-05 13:21:02 +0900

    영화 데모 앱 업데이트
    tv 프로그램 페이지, tv 프로그램 상세 페이지 추가

--- src/pages/Tv.jsx
+++ src/pages/Tv.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { tvDummy } from '../tvDummy';
+import Tvprogram from '../components/Tvprogram';
 
 export default function Tv() {
   return (
-    <div className='page-container' style={{fontSize: '32px'}}>Tv</div>
-  )
+    <div className='tvs-container' style={{fontSize: '32px'}}>
+      {tvDummy.results.map((item) => {
+        return(
+          <Tvprogram
+            name={item.name}
+            poster_path={item.poster_path}
+            vote_average={item.vote_average}
+            overview = {item.overview}
+            first_air_date = {item.first_air_date}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/TvDetail.jsx
+++ src/pages/TvDetail.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { tvDummy } from '../tvDummy'
-import Tvprogram from '../components/Tvprogram'
 import { IMG_BASE_TV_URL } from '../components/Tvprogram'
 import { useLocation, useParams } from 'react-router-dom'
 
 export default function TvDetail() {
     const { name } = useParams();
     const { state } = useLocation();
+    const rating = state.vote_average.toFixed(1);
+    const overview = state.overview || "줄거리 정보가 없습니다";
+    const first_air_date = state.first_air_date || "첫 방영일 정보가 없습니다";
 
   return (
-    <div className='tv-detail-container' style={{fontSize: '32px'}}>
-        {tvDummy.results.map((item) => {
-        return(
-            <Tvprogram
-            
-            />
-        );
-        })}
+    <div className='tv-detail-container'>
+        <img
+            className='tv-detail-poster'
+            src={IMG_BASE_TV_URL + state.poster_path}
+            alt='티비 포스터 이미지'
+         />
+         <div className='tv-detail-info'>
+            <h1 className='tv-detail-name'>{name}</h1>
+            <p className='tv-detail-date'>First Air Date: {first_air_date}</p>
+            <div className='tv-detail-rating'>⭐ {rating} / 10</div>
+            <p className='tv-detail-overview'>{overview}</p>
+         </div>
     </div>
-)
+  );
 }
